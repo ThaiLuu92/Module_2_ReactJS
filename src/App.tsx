7872bf7f3b16d1cc44ea45f6edc25218f212ebde
ThaiLuu92 <matienjp92@gmail.com>
2023-09-28 18:36:49 +0700

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,128 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import "./App.css"
+import { users } from './models/data';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  hanldeDeleteUser(index: number) {
+    users.splice(index, 1);
+    console.log(users);
+    
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title text-uppercase mb-0">MANAGER USER</h5>
+              </div>
+              <div className="table-responsive">
+                <table className="table no-wrap user-table mb-0">
+                  <thead>
+                    <tr>
+                      <th
+                        scope="col"
+                        className="border-0 text-uppercase font-medium pl-4"
+                      />
+                      <th
+                        scope="col"
+                        className="border-0 text-uppercase font-medium"
+                      >
+                        Name
+                      </th>
+                      <th
+                        scope="col"
+                        className="border-0 text-uppercase font-medium"
+                      >
+                        Work
+                      </th>
+                      <th
+                        scope="col"
+                        className="border-0 text-uppercase font-medium"
+                      >
+                        Email
+                      </th>
+                      <th
+                        scope="col"
+                        className="border-0 text-uppercase font-medium"
+                      >
+                        Dateofbirth
+                      </th>
+                      <th
+                        scope="col"
+                        className="border-0 text-uppercase font-medium"
+                      >
+                        Manage
+                      </th>
+                      <th
+                        scope="col"
+                        className="border-0 text-uppercase font-medium"
+                      >
+                        Action
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {users.map((user, index) => (
+                      <tr>
+                        <td className="pl-4">{index + 1}</td>
+                        <td>
+                          <h6 className="font-medium mb-0">{user.name}</h6>
+                        </td>
+                        <td>
+                          <span className="text-muted">{user.work}</span>
+                          {/* <br />
+                        <span className="text-muted">Past : teacher</span> */}
+                        </td>
+                        <td>
+                          <span className="text-muted">{user.email}</span>
+                          <br />
+                        </td>
+                        <td>
+                          <span className="text-muted">
+                            {new Date(user.age).toLocaleDateString()}
+                          </span>
+                          <br />
+                        </td>
+                        <td>
+                          <select
+                            className="form-control category-select"
+                            id="exampleFormControlSelect1"
+                          >
+                            <option>Modulator</option>
+                            <option>Admin</option>
+                            <option>User</option>
+                            <option>Subscriber</option>
+                          </select>
+                        </td>
+                        <td>
+                          <button
+                            type="button"
+                            className="btn btn-outline-info btn-circle btn-lg btn-circle ml-2"
+                            onClick={() => this.hanldeDeleteUser(index)}
+                          >
+                            <i className="fa fa-trash" />{" "}
+                          </button>
+                          <button
+                            type="button"
+                            className="btn btn-outline-info btn-circle btn-lg btn-circle ml-2"
+                          >
+                            <i className="fa fa-edit" />{" "}
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/models/data.ts
@@ -0,0 +1,44 @@
+export const users = [
+  {
+    name: "Ms. Wendell Muller",
+    age: "1996-06-04T08:21:55.228Z",
+    email: "Roman_Dooley@gmail.com",
+    work: "Legacy Applications Executive",
+    id: "1",
+  },
+  {
+    name: "Rita McLaughlin",
+    age: "1952-07-29T22:39:59.554Z",
+    email: "Sydney70@yahoo.com",
+    work: "Global Implementation Facilitator",
+    id: "2",
+  },
+  {
+    name: "Gladys Beatty",
+    age: "1944-01-03T21:47:15.025Z",
+    email: "Audrey.Brekke@yahoo.com",
+    work: "Internal Markets Facilitator",
+    id: "3",
+  },
+  {
+    name: "Mrs. Ira O'Hara",
+    age: "1952-03-31T14:38:11.121Z",
+    email: "Devin27@gmail.com",
+    work: "Lead Brand Assistant",
+    id: "4",
+  },
+  {
+    name: "Naomi O'Kon",
+    age: "1953-08-05T21:10:48.298Z",
+    email: "Nicola.Douglas13@hotmail.com",
+    work: "Customer Program Liaison",
+    id: "5",
+  },
+  {
+    name: "Leslie Kertzmann",
+    age: "1972-09-12T13:14:18.603Z",
+    email: "Kris54@yahoo.com",
+    work: "Direct Web Consultant",
+    id: "6",
+  },
+];
